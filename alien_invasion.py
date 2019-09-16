b8686922c590e9dd4ac7705e89dda247b080a2ae
maplerivertree <44853896+maplerivertree@users.noreply.github.com>
2019-09-15 21:40:24 -0700

    Add files via upload

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,10 +1,18 @@
 import sys
 import pygame
 
+from settings import settings
+
 def run_game():
 	#Initialize game and create a screen object
 	pygame.init()
-	screen = pygame.display.set_mode((1200,800))
+	
+	ai_settings = Settings()
+
+	# screen = pygame.display.set_mode((1200, 800)) but we pull from Class:
+	screen = pygame.display.set_mode(
+		(ai_settings.screen_width, ai_settings.screen_height))
+	
 	pygmae.display.set_caption("Alien Invasion")
 
 	# Start the main loop for the game
@@ -14,6 +22,10 @@ def run_game():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit()
+
+		# Redraw the screen during each pass through the loop
+		screen.fill(ai_settings.bg_color)
+
 
 		# Make the most recently drawn screen visible
 		pygame.display.flip()
